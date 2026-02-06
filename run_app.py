fb165ae8af5cbddfd51d8b255ab3340656e3227a
Julian Le Gouic <julian.legouic@gmail.com>
2026-02-06 17:24:02 +0100

    add creation of raw and mask sub temp folder

--- run_app.py
+++ run_app.py
@@ -6,5 +6,7 @@ from src.utils import makedirs
 if __name__ == '__main__':
     # Clear tmp directory from previous session
     makedirs("tmp", exist_ok=False)
+    makedirs("tmp/raw", exist_ok=False)
+    makedirs("tmp/mask", exist_ok=False)
     sys.argv = ["streamlit", "run", "src/ui/app.py"]
     sys.exit(stcli.main())
