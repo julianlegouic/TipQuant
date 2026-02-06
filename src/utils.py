72b49bab61a78bb3d18b21c4f379dd3c89288bde
Julian Le Gouic <julian.legouic@gmail.com>
2026-02-06 15:51:24 +0100

    add function to clear content of directory

--- src/utils.py
+++ src/utils.py
@@ -25,6 +25,20 @@ def makedirs(path, exist_ok=True):
             os.remove(os.path.join(path, f))
 
 
+def clear_directory(dir_path):
+    """Clear all the content of a directory."""
+    if os.path.exists(dir_path):
+        for filename in os.listdir(dir_path):
+            file_path = os.path.join(dir_path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f'Failed to delete {file_path}. Reason: {e}')
+
+
 def load_video(b_io, video_file):
     """
     loads video to the BytesIO object
